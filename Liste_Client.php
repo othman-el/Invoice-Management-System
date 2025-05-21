7b82a6cc154d68664b660b6d6a15067e11600c36
Hichamelhmaydi <ct1594065@gmail.com>
2025-05-21 15:56:49 +0100

    ajouter les client et fornisseur

--- Liste_Client.php
+++ Liste_Client.php
@@ -1,20 +1,77 @@
+<?php
+include_once 'Database.php';
+$sql = "SELECT ID,NameEntreprise,ICE,Adresse,Email,Contact,NumeroGSM,NumeroFixe,Activite FROM users where role ='Client' " ;
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet"
-      type="text/css">
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet"
+        type="text/css">
     <title>Document</title>
 </head>
-     <?php
+<?php
        include './front/head_front.php';
      ?>
-      <div class="container py-2">
-      <a href="ajouter_fourniseur.php" class="btn btn-primary"><i class="fa-solid fa-user-plus"></i>
-      Ajouter Client</a>
-     </div>
+<div class="container py-2">
+    <a href="ajouter_client.php" class="btn btn-primary"><i class="fa-solid fa-user-plus"></i>
+        Ajouter Client</a>
+</div>
+<table class="table table-striped table-bordered">
+    <thead class="bg-primary text-center">
+        <tr>
+            <th class="text-white">ID</th>
+            <th class="text-white">Nom de l'entreprise</th>
+            <th class="text-white">ICE</th>
+            <th class="text-white">Adresse</th>
+            <th class="text-white">Email</th>
+            <th class="text-white">Contact</th>
+            <th class="text-white">NumeroGSM</th>
+            <th class="text-white">NumeroFixe</th>
+            <th class="text-white">Activite</th>
+            <th class="text-white">Action</th>
+
+        </tr>
+    </thead>
+    <tbody>
+        <?php if (count($users) > 0): ?>
+        <?php foreach ($users as $user): ?>
+        <tr>
+            <td class=" text-center"><?= htmlspecialchars($user['ID']) ?></td>
+            <td><?= htmlspecialchars($user['NameEntreprise']) ?></td>
+            <td><?= htmlspecialchars($user['ICE']) ?></td>
+            <td><?= htmlspecialchars($user['Adresse']) ?></td>
+            <td><?= htmlspecialchars($user['Email']) ?></td>
+            <td><?= htmlspecialchars($user['Contact']) ?></td>
+            <td><?= htmlspecialchars($user['NumeroGSM']) ?></td>
+            <td><?= htmlspecialchars($user['NumeroFixe']) ?></td>
+            <td><?= htmlspecialchars($user['Activite']) ?></td>
+            <td class="text-center">
+                <a href="modifier_utilisateur.php?id=<?= $user['ID'] ?>" class="btn btn-sm btn-warning me-1"
+                    title="Modifier">
+                    <i class="fa-solid fa-pen"></i>
+                </a>
+                <a href="supprimer_utilisateur.php?id=<?= $user['ID'] ?>" class="btn btn-sm btn-danger"
+                    title="Supprimer" onclick="return confirm('Voulez-vous vraiment supprimer cet utilisateur ?');">
+                    <i class="fa-solid fa-trash"></i>
+                </a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+        <?php else: ?>
+        <tr>
+            <td colspan="4" class="text-center">Aucun utilisateur trouvé.</td>
+        </tr>
+        <?php endif; ?>
+    </tbody>
+</table>
 </body>
+
 </html>
